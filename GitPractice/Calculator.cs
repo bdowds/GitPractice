3399667c330a2bc2e71779270493a0d4ff85ce99
bdowds <bwdowds@gmail.com>
2018-11-15 19:09:48 -0600

    Changed paramater types of Multiply Method

--- GitPractice/Calculator.cs
+++ GitPractice/Calculator.cs
@@ -30,7 +30,7 @@ namespace GitPractice
             return num1 - num2;
         }
 
-        public int Multiply(int num1, int num2)
+        public double Multiply(double num1, double num2)
         {
             return num1 * num2;
         }
