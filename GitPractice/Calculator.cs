7c071ca10ff631ec3fd68fd52c51e5e3c15abcaf
bdowds <bwdowds@gmail.com>
2018-11-15 19:05:59 -0600

    Added comments

--- GitPractice/Calculator.cs
+++ GitPractice/Calculator.cs
@@ -6,6 +6,20 @@ namespace GitPractice
 {
     class Calculator
     {
+        //Create project and git init
+        //Initial commit : git commit -m"Initial Commit"
+        //Made Changes
+        //Made more commits
+        //pushed to gitHub: git push
+        //created a branch: git branch BranchName
+        //Switched to branch: git checkout BranchName
+        //Pull Request
+        //Merged the pull request
+        //Pulled updated master branch : git pull
+        //Deleted branch: git branch -d BranchName
+
+
+
         public int Add(int num1, int num2)
         {
             return num1 + num2;
