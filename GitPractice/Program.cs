57586eee3c823cbde568e3b13adc69f27e93ac3e
bdowds <bwdowds@gmail.com>
2018-11-15 20:14:17 -0600

    Refactored Main Method

--- GitPractice/Program.cs
+++ GitPractice/Program.cs
@@ -6,45 +6,50 @@ namespace GitPractice
     {
         static void Main(string[] args)
         {
-            Calculator calc = new Calculator();
-
-            Console.WriteLine("Number 1: ");
-            int number1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Number 2: ");
-            int number2 = int.Parse(Console.ReadLine());
-
             int choice = 0;
             double result = 0;
+            Calculator calc = new Calculator();
 
-            Console.WriteLine("What would you like to do with these numbers?");
-            Console.WriteLine("1.Add");
-            Console.WriteLine("2.Subtract");
-            Console.WriteLine("3.Multiply");
-            Console.WriteLine("4.Divide");
-
-            choice = int.Parse(Console.ReadLine());
-
-            switch(choice)
+            while(choice != 5)
             {
-                case 2:
-                    result = calc.Subtract(number1, number2);
-                    break;
-                case 1:
-                    result = calc.Add(number1, number2);
-                    break;
-                case 3:
-                    result = calc.Multiply(number1, number2);
-                    break;
-                case 4:
-                    result = calc.Divide(number1, number2);
-                    break;
-                default:
-                    Console.WriteLine("Not a valid choice");
-                    break;
-            }
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("1.Add");
+                Console.WriteLine("2.Subtract");
+                Console.WriteLine("3.Multiply");
+                Console.WriteLine("4.Divide");
+                Console.WriteLine("5.Exit");
 
-            Console.WriteLine($"Result: {result}");
+                choice = int.Parse(Console.ReadLine());
 
+                if(choice != 5)
+                {
+                    Console.WriteLine("Number 1: ");
+                    int number1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Number 2: ");
+                    int number2 = int.Parse(Console.ReadLine());
+
+                    switch (choice)
+                    {
+                        case 2:
+                            result = calc.Subtract(number1, number2);
+                            break;
+                        case 1:
+                            result = calc.Add(number1, number2);
+                            break;
+                        case 3:
+                            result = calc.Multiply(number1, number2);
+                            break;
+                        case 4:
+                            result = calc.Divide(number1, number2);
+                            break;
+                        default:
+                            Console.WriteLine("Not a valid choice");
+                            break;
+                    }
+
+                    Console.WriteLine($"Result: {result}");
+                }  
+            }          
         }
     }
 }
